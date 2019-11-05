75dd79ece52d620368f6de76ee01887d5f44f895
Ayberk Anil ATSIZ <anil@monkeysteam.com>
2019-11-05 10:36:02 +0300

    userId method added

--- instagram.js
+++ instagram.js
@@ -822,4 +822,14 @@ ig_cb=1
     }).then(t => t.json().then(r => r));
   }
 
+  getUserInfoFromId(userId){
+    return fetch(`https://i.instagram.com/api/v1/users/${userId}/info/`, {
+      
+      'method': 'get',
+      'headers':{
+        "User-Agent":"Mozilla/5.0 (iPhone; CPU iPhone OS 12_3_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 Instagram 105.0.0.11.118 (iPhone11,8; iOS 12_3_1; en_US; en-US; scale=2.00; 828x1792; 165586599)"
+      }
+    }).then(t => t.json().then(r => r));
+  }
+
 }
